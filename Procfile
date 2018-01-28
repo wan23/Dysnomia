a459933c37e0cedf05767698990702464b8dd8db
Juan Walker <juan@Juans-MacBook-Pro-2.local>
2018-01-28 14:59:42 -0500

    stuff

---
+++ Procfile
@@ -0,0 +1,5 @@
+web: gunicorn app:app
+
+init: python manage.py db init
+migrate: python manage.py db migrate
+upgrade: python manage.py db upgrade
---
+++ manage.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import app
+from app.database import *
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
